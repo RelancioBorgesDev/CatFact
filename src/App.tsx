528bac3d6d15c0c4ebfd51ca668f5e6f6b80300b
RelancioBorgesDev <relancio.dev@gmail.com>
2022-08-16 17:01:47 -0300

    end: added and consumed the  catfact api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import './styles/globals.css'
-import styles from './styles/styles.module.css';
+import "./styles/globals.css";
+import styles from "./styles/styles.module.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  
+  const [phrase, setPhrase] = useState<string>(
+    "Click in the button to generate, a fact about cats!"
+  );
+  const [factPhrase, setFactPhrase] = useState<string>("");
+
+  useEffect(() => {
+    const res = axios.get("https://catfact.ninja/fact");
+    res.then((data) => setFactPhrase(data.data.fact));
+  }, [phrase]);
+
+  function onGenerate(e: any) {
+    setPhrase(factPhrase);
+  }
+
   return (
     <div className={styles.container}>
       <div>
-        <p>Click in the button to generate, a fact about cats!</p>
-        <button>Generate Fact</button>
+        <p>{phrase}</p>
+        <button onClick={onGenerate}>Generate Fact</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
